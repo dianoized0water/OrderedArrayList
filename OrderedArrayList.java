af4331568e05979d4bf2b462bf3c9a361cfe1c9a
diana_yang <dyang20@stuy.edu>
2020-12-09 18:24:54 -0500

    added value and the first add method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -8,4 +8,25 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   OrderedArrayList(int InitialCapacity){
     super(InitialCapacity);
   }
+
+  private int where(T thing){
+    int a = 0;
+    for (int i=0; i<this.size(); i++){
+      if (thing.compareTo(this.get(i))<=0){
+        return a;
+      }
+      else{
+        a++;
+      }
+    }
+    return a;
+  }
+
+  public boolean add(T thing){
+    if (thing==null){
+      throw new IllegalArgumentException();
+    }
+    super.add(where(thing), thing);
+    return true;
+  }
 }
