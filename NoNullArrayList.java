3e44831c5c12f41f54dd2004549e1325bf5da7fd
diana_yang <dyang20@stuy.edu>
2020-12-09 17:45:21 -0500

    first method compiles

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -8,4 +8,18 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   NoNullArrayList(int InitialCapacity){
     super(InitialCapacity);
   }
+
+  private boolean isNull(T added){
+    return added==null;
+  }
+
+  public boolean add(T thing){
+    if (isNull(thing)){
+      throw new IllegalArgumentException();
+    }
+    else {
+      super.add(thing);
+    }
+    return true;
+  }
 }
